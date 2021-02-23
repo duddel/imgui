d5f02a80fdbd9c94a467a557f20353f3959a865a
duddel <duddel@users.noreply.github.com>
2021-02-23 11:22:39 +0100

    Try assemble via gradle, without wrapper

--- examples/example_android_opengl3/build_android.sh
+++ examples/example_android_opengl3/build_android.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 cd android
 
-cd gradle/wrapper
-curl --location --output gradle-wrapper.jar.sha256 \
-       https://services.gradle.org/distributions/gradle-5.4.1-wrapper.jar.sha256
-echo "  gradle-wrapper.jar" >> gradle-wrapper.jar.sha256
-sha256sum --check gradle-wrapper.jar.sha256
-cd ../..
+#cd gradle/wrapper
+#curl --location --output gradle-wrapper.jar.sha256 \
+#       https://services.gradle.org/distributions/gradle-5.4.1-wrapper.jar.sha256
+#echo "  gradle-wrapper.jar" >> gradle-wrapper.jar.sha256
+#sha256sum --check gradle-wrapper.jar.sha256
+#cd ../..
 
-./gradlew assembleDebug
+#./gradlew assembleDebug
+
+gradle assembleDebug
