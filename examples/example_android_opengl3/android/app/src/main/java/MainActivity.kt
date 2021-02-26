5c92d67283bf27da88416fc3b4a73ee42ab4bc79
duddel <duddel@users.noreply.github.com>
2021-02-26 13:40:36 +0100

    Android: Simplify Kotlin code, fix warnings

--- examples/example_android_opengl3/android/app/src/main/java/MainActivity.kt
+++ examples/example_android_opengl3/android/app/src/main/java/MainActivity.kt
@@ -13,38 +13,28 @@ class MainActivity : NativeActivity() {
     }
 
     fun showSoftInput() {
-        val input_method_manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        input_method_manager.showSoftInput(this.window.decorView, 0)
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(this.window.decorView, 0)
     }
 
     fun hideSoftInput() {
-        val input_method_manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        input_method_manager.hideSoftInputFromWindow(this.window.decorView.windowToken, 0)
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(this.window.decorView.windowToken, 0)
     }
 
     // Queue for the Unicode characters to be polled from native code (via pollUnicodeChar())
-    private var UnicodeCharacterQueue: LinkedBlockingQueue<Int> = LinkedBlockingQueue()
+    private var unicodeCharacterQueue: LinkedBlockingQueue<Int> = LinkedBlockingQueue()
 
     // We assume dispatchKeyEvent() of the NativeActivity is actually called for every
     // KeyEvent and not consumed by any View before it reaches here
     override fun dispatchKeyEvent(event: KeyEvent): Boolean {
-        var unicode_character = event.getUnicodeChar(event.metaState)
-
         if (event.action == KeyEvent.ACTION_DOWN) {
-            if (unicode_character != 0) {
-                UnicodeCharacterQueue.offer(Integer.valueOf(unicode_character))
-            } else {
-                UnicodeCharacterQueue.offer(Integer.valueOf(0))
-            }
-        } else if (event.action == KeyEvent.ACTION_MULTIPLE) {
-            unicode_character = Character.codePointAt(event.characters, 0)
-            UnicodeCharacterQueue.offer(Integer.valueOf(unicode_character))
+            unicodeCharacterQueue.offer(event.getUnicodeChar(event.metaState))
         }
-
         return super.dispatchKeyEvent(event)
     }
 
     fun pollUnicodeChar(): Int {
-        return if (!UnicodeCharacterQueue.isEmpty()) UnicodeCharacterQueue.poll().toInt() else 0
+        return unicodeCharacterQueue.poll() ?: 0
     }
 }
